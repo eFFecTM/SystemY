cb29e45ff433b53c4f4428fa8fd9f706732a7dc9
thomasjanssenR8 <r8.thomas@hotmail.com>
2016-10-25 11:32:57 +0200

    Hash thomas 3

--- src/JJTP_DS_UA/Main.java
+++ src/JJTP_DS_UA/Main.java
@@ -5,13 +5,10 @@ public class Main
 
     public static void main(String[] args)
     {
-        // Hello world!
-        String name = "Jonas";
+        Node node = new Node("jonas","192.168.1.2");
+        String nodeName = node.getName();
         NameServer naming = new NameServer();
-        naming.addName(name);
+        naming.addName(nodeName);
 
     }
 }
-// jonas aanpas test : thomas is homo
-// thomas aanpas test: jonas is een grotere homo
-// Peter aanpas test : Allemaal homo's
