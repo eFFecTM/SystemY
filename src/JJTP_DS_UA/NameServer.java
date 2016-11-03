eeee2e1d6017eb4165e9d7d229f4bc500365046d
killerboye <jan.delaet@hotmail.com>
2016-11-03 14:13:24 +0100

    nodeMap naar XML

--- src/JJTP_DS_UA/NameServer.java
+++ src/JJTP_DS_UA/NameServer.java
@@ -1,5 +1,9 @@
 package JJTP_DS_UA;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import java.io.File;
 import java.net.Inet4Address;
 import java.net.MalformedURLException;
 import java.rmi.AlreadyBoundException;
@@ -34,7 +38,7 @@ public class NameServer
         }
     }
 
-    public void addName(String name, Inet4Address IP)
+    public void addNode(String name, Inet4Address IP)
     {
         Integer hashCode = name.hashCode();
         Integer hash = (int) Integer.toUnsignedLong(hashCode) % 32768;
@@ -48,6 +52,24 @@ public class NameServer
             return nodeMap.get(nodeMap.lastKey()); //returnt de grootste key uit de map
         else
             return nodeMap.get(nodeMap.lowerKey(fileNameHash));
+    }
+
+    public void saveNodeMapXML()
+    {
+        try
+        {
+            File file = new File("NodeMap.xml");
+            //Marshalling
+            JAXBContext context = JAXBContext.newInstance(TreeMap.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            m.marshal(nodeMap,file);
+            System.out.println("Converted to XML.");
+        }
+        catch (JAXBException e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
