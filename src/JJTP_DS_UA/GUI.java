bd2cc156f550ae0bc32831b0ffdc9c9aaff21053
Jan De Laet <jan.delaet@hotmail.com>
2016-11-11 16:06:04 +0100

    GUI verbeterd

--- src/JJTP_DS_UA/GUI.java
+++ src/JJTP_DS_UA/GUI.java
@@ -1,8 +1,18 @@
 package JJTP_DS_UA;
 
+import javax.swing.*;
+
 /**
  * Created by Thomas on 11/11/2016.
  */
 public class GUI
 {
+    private JTextArea welcomeToSystemYTextArea;
+    private JTextField loginName;
+    private JTextArea file1;
+    private JTextArea file2;
+    private JTextArea file5;
+    private JTextArea file3;
+    private JTextArea file4;
+    private JButton button1;
 }
