1825da4b82ded6b4e842f7200da3267d53f235bd
Jonas <derynck.games@gmail.com>
2016-12-22 16:44:09 +0100

    FileAgent fix v2  currentnode

--- src/JJTP_DS_UA/FileAgent.java
+++ src/JJTP_DS_UA/FileAgent.java
@@ -24,6 +24,7 @@ public class FileAgent implements Runnable,Serializable
     {
         updateSystemYfiles();
         updateNodeSystemYfiles();
+        currentNode = null;
         //checkLocks();
     }
 
